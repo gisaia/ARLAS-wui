ceef44d44fac66f5c6e0917c5fcb81e07821ae6a
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2017-11-09 12:22:34 +0100

    new theme for the app

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,7 +1,11 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, Input } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { AggregationResponse, Aggregation, Filter } from 'arlas-api';
 import { ArlasWuiCollaborativesearchService } from '../../services/arlaswui.startup.service';
 import { ContributorService } from '../../services/contributors.service';
+import { projType } from 'arlas-web-core';
+import { Response } from '@angular/http';
 
 
 @Component({
@@ -11,41 +15,62 @@ import { ContributorService } from '../../services/contributors.service';
 })
 export class SearchComponent {
   public sizeOnBackspaceBus: Subject<boolean> = new Subject<boolean>();
-  public searchedWords = new Array<{display: string, value: string}>();
-  public searchedWordsSet = new Set<{display: string, value: string}>();
+  public searchedWords = new Array<{ display: string, value: string }>();
 
   @Output() public wordAddedEvent: Subject<string> = new Subject<string>();
   @Output() public wordRemovedEvent: Subject<string> = new Subject<string>();
 
+  public listItems = new Array<any>();
 
-  constructor(private collaborativesearchService: ArlasWuiCollaborativesearchService,
+  constructor(private collaborativeSearchService: ArlasWuiCollaborativesearchService,
     private contributorService: ContributorService) {
-      this.subscribeToSearchedWordsInBookMarks();
+    this.subscribeToSearchedWordsInBookMarks();
   }
 
-  public onWordAdded(word: {display: string, value: string}) {
+  public onWordAdded(word: { display: string, value: string }) {
     this.wordAddedEvent.next(word.value);
   }
 
-  public onWordRemoved(word: {display: string, value: string}) {
+  public onWordRemoved(word: { display: string, value: string }) {
     this.wordRemovedEvent.next(word.value);
   }
 
 
   private subscribeToSearchedWordsInBookMarks() {
-    this.collaborativesearchService.contribFilterBus
-      .filter(contributor => contributor.identifier === this.contributorService.CHIPSSEARCH_ID ).first().subscribe(contributor => {
-        const searchedWords = contributor.getFilterDisplayName();
-        if (searchedWords !== undefined) {
-          const searchedWordsSplited = Array.from(searchedWords.split(' '));
-          this.searchedWordsSet = new Set<{display: string, value: string}>();
+    this.collaborativeSearchService.contribFilterBus
+      .filter(contributor => contributor.identifier === this.contributorService.CHIPSSEARCH_ID).first().subscribe(contributor => {
+        const storedSearchWords = contributor.getFilterDisplayName();
+        if (storedSearchWords !== undefined) {
+          const searchedWordsSplited = storedSearchWords.split(' ');
           searchedWordsSplited.forEach(word => {
             if (word !== '') {
-              this.searchedWordsSet.add({display: word, value: word});
+              this.searchedWords.push({ display: word, value: word });
             }
           });
-          this.searchedWords = Array.from(this.searchedWordsSet);
         }
       });
   }
+
+  public filterSearch(search: string): void {
+    const aggregation: Aggregation = {
+      type: Aggregation.TypeEnum.Term,
+      field: 'callsign',
+      include: search.toUpperCase() + '.*',
+      size: '10'
+    };
+    const filter: Filter = {
+      q: 'callsign' + ':' + search.toUpperCase() + '*'
+    };
+    this.collaborativeSearchService.resolveButNotAggregation([projType.aggregate, [aggregation]], null, filter).subscribe(
+      results => {
+        this.listItems = new Array<any>();
+        if (results.elements != null) {
+          results.elements.forEach(elem => {
+            this.listItems.push(elem.key);
+          });
+        }
+      }
+    );
+  }
+
 }
