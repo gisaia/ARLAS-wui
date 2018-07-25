5bfa31e7f87a299586a3b6f138db2d9c8877a464
MohamedHamou <mohamed.hamou@gisaia.com>
2018-07-25 16:50:52 +0200

    Add geoAutosort between map and resultlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, group } from '@angular/core';
 import { Http } from '@angular/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Filter } from 'arlas-api';
@@ -68,6 +68,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     elementidentifier: ElementIdentifier
   };
   public featuresToSelect: Array<ElementIdentifier> = [];
+  private isAutoGeosortActive;
 
 
   @ViewChild('map') private mapglComponent: MapglComponent;
@@ -100,6 +101,15 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   public ngAfterViewInit(): void {
     this.mapglComponent.switchLayer.subscribe(data => this.mapglContributor.switchLayerCluster(data));
+    this.mapglComponent.map.on('moveend', () => {
+      if (this.isAutoGeosortActive === undefined) {
+        this.analytics.filter(g => g.groupId === 'resultlist')
+          .map(g => this.isAutoGeosortActive = g.components[0].input.isAutoGeoSortActived);
+      }
+      if (this.isAutoGeosortActive) {
+        this.resultlistContributor.geoSort(this.mapglComponent.map.getCenter().lat, this.mapglComponent.map.getCenter().lng);
+      }
+    });
   }
 
   public filterSearch(value: string) {
@@ -149,7 +159,10 @@ export class AppComponent implements OnInit, AfterViewInit {
             break;
           case 'geoSortEvent':
             this.resultlistContributor.geoSort(this.mapglComponent.map.getCenter().lat, this.mapglComponent.map.getCenter().lng);
-          break;
+            break;
+          case 'geoAutoSortEvent':
+            this.isAutoGeosortActive = event.data;
+            break;
         }
 
         break;
