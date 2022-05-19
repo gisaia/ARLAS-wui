c730390ab638c0dded8633bd159ab2c4524ac104
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2022-05-19 22:59:49 +0200

    Add donut test

--- cypress/integration/006_donut_spec.js
+++ cypress/integration/006_donut_spec.js
@@ -2,9 +2,20 @@ describe('Analytics', () => {
 
   it('Donut', () => {
 
-    // hide analytics
-    cy.get('#svgix .donut__arc--container > :nth-child(7)').click({force: true});
+    // move collection group up
+    cy.get('#mat-expansion-panel-header-1')
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientY: -200 })
+      .trigger('mouseup', { force: true });
+
+    cy.wait(500);
+    cy.get('#svgix .donut__arc--container > :nth-child(7)').click({ force: true });
     cy.wait(2000);
+
+    // assert the value
+    cy.get('#arlas-count .value').should(elem => {
+      expect(elem.text().trim()).to.equal('4 545');
+    });
 
   });
 });
