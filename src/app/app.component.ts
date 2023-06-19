056a639065709e75b51c0d2a47c598d3053c645c
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2023-06-19 17:45:01 +0200

    feat: add top menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,7 +51,7 @@ import { VisualizeService } from './services/visualize.service';
 @Component({
   selector: 'arlas-wui-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class ArlasWuiComponent implements OnInit, AfterViewInit {
   @Input() public version: string;
