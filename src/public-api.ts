80378d8ab5c071fc28fe11ce1416dd53e2a02ce5
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2023-09-11 13:32:16 +0200

    development version 24.2.0-dev

--- src/public-api.ts
+++ src/public-api.ts
@@ -6,3 +6,4 @@ export { AboutDialogComponent } from './app/components/about/about.component';
 export { ConfigsListComponent } from './app/components/configs-list/configs-list.component';
 export { LeftMenuComponent } from './app/components/left-menu/left-menu.component';
 export { ArlasTranslateLoader }  from './app/tools/customLoader';
+export { ArlasWuiComponent } from './app/app.component';
