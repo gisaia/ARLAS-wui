58c28189ffeb1fca1818cc823fde9279814c94f6
Mohamed Hamou <mohamed.hamou@gisaia.com>
2023-09-22 16:22:55 +0200

    Add forms to set coordinates

--- src/app/components/bbox-generator/bbox-generator.component.ts
+++ src/app/components/bbox-generator/bbox-generator.component.ts
@@ -17,16 +17,76 @@
  * under the License.
  */
 
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, merge, mergeMap, of } from 'rxjs';
 
 @Component({
   selector: 'arlas-bbox-generator',
   templateUrl: './bbox-generator.component.html',
   styleUrls: ['./bbox-generator.component.scss']
 })
-export class BboxGeneratorComponent {
-
+export class BboxGeneratorComponent implements OnInit, AfterViewInit {
   public DESCRIPTION = 'Enter coordinates in decimal or sexagesimal degrees';
+  public bboxForm: BboxFormGroup;
+
+  public constructor(private cdr: ChangeDetectorRef
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.bboxForm = new BboxFormGroup();
+  }
+  public ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+    console.log(this.bboxForm);
+  }
+
+}
+
+export class BboxFormGroup extends FormGroup {
+  public firstCorner: PointFormGroup;
+  public secondCorner: PointFormGroup;
+
+  public constructor() {
+    const firstCorner = new PointFormGroup();
+    const secondCorner = new PointFormGroup();
+    super({
+      firstCorner,
+      secondCorner
+    });
+    this.firstCorner = firstCorner;
+    this.secondCorner = secondCorner;
+
+  }
+}
+
+export class PointFormGroup extends FormGroup {
+
+  public latitude: FormControl;
+  public longitude: FormControl;
+
+  public latitudeChanges$: Observable<any>;
+  public longitudesChanges$: Observable<any>;
+
+  public constructor() {
+    const latitude = new FormControl( );
+    const longitude = new FormControl();
+    super({
+      latitude,
+      longitude
+    });
+    this.latitude = latitude;
+    this.longitude = longitude;
+
+    merge(this.latitude.valueChanges, this.longitude.valueChanges).pipe(
+      // mergeMap(v => {
+      //   return v;
+      // })
+    ).subscribe(r => console.log(r));
+
+    this.latitude.valueChanges.subscribe(r => console.log(r));
+  }
 }
 
 
