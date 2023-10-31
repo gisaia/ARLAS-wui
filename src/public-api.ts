6d13d143f7ad9c628d57ff80129e8ad318ae0aa9
Mohamed Hamou <mohamed.hamou@gisaia.com>
2023-10-31 08:37:37 +0100

    Add missing pipes/components in library entrypoint

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,4 +4,6 @@ export { ArlasWuiRootComponent} from './app/components/arlas-wui-root/arlas-wui-
 export { ConfigsListComponent } from './app/components/configs-list/configs-list.component';
 export { LeftMenuComponent } from './app/components/left-menu/left-menu.component';
 export { ArlasTranslateLoader }  from './app/tools/customLoader';
+export { AoiDimensionComponent } from './app/components/map/aoi-dimensions/aoi-dimensions.component';
+export { RoundKilometer, SquareKilometer } from './app/components/map/aoi-dimensions/aoi-dimensions.pipes';
 export { ArlasWuiComponent } from './app/app.component';
