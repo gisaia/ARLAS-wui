0c2f2cc94ae85087ae4bab0f8e963b1e241f4ba1
Mickael Audel <mickael.audel@gisaia.com>
2024-01-26 18:05:29 +0100

    work: improve visualisation for finest

---
+++ src/config_improved.ts
@@ -0,0 +1,171 @@
+const heatmap = {
+  "id": "arlas_id:Density of birds positions:1624986445203",
+  "type": "heatmap",
+  "source": "cluster-track.location-Finest-geohash-centroid-demo_birdtracking-movebank",
+  "minzoom": 0,
+  "maxzoom": 23,
+  "layout": {
+    "visibility": "visible"
+  },
+  "paint": {
+    "heatmap-color": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "heatmap-density"
+      ],
+      0, "rgba(0, 0, 0, 0)",
+      1e-12, "#cee897",
+      0.05, "#9dc95b",
+      0.1, "#4bc71e",
+      0.15, "#1bc473",
+      0.2, "#16ccc9",
+      0.25, "#3619cf",
+      0.3, "#9317d1",
+      0.9, "#d11799",
+      1, "#cf1919"
+    ],
+    "heatmap-opacity": 0.7,
+    "heatmap-intensity": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      ["zoom"
+      ],
+      0, 1,
+      10, 5
+    ],
+    "heatmap-weight": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0, 0,
+      0.1, 1,
+      0.2, 1,
+      0.5, 2,
+      0.9, 10
+    ],
+    "heatmap-radius": 12
+  },
+  "metadata": {
+    "collection": "demo_birdtracking-movebank",
+    "collection-display-name": "demo_birdtracking-movebank"
+  },
+  "filter": [
+    "all",
+    [
+      "all"
+    ]
+  ]
+}
+
+
+const cirlce_fines = {
+  "id": "arlas_id:Gathered birds positions circle:1706023503869",
+  "type": "circle",
+  "source": "cluster-track.location-Finest-geohash-cell_center-demo_birdtracking-movebank",
+  "minzoom": 0,
+  "maxzoom": 24,
+  "layout": {
+    "visibility": "visible",
+    "circle-sort-key": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0, 0,
+      0.2, 2,
+      0.4, 4,
+      0.6, 8
+    ]
+  },
+  "paint": {
+    "circle-opacity": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0, 0.2,
+      1,0.5
+    ],
+    "circle-color": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0,
+      "#cee897",
+      0.05,
+      "#9dc95b",
+      0.1,
+      "#4bc71e",
+      0.15,
+      "#1bc473",
+      0.2,
+      "#16ccc9",
+      0.25,
+      "#3619cf",
+      0.3,
+      "#9317d1",
+      0.6,
+      "#d11799",
+      1,
+      "#cf1919"
+    ],
+    "circle-radius":  [
+      "interpolate",
+      [
+        "linear"
+      ],
+      ["zoom"],
+      0, 9,
+      2.9999999999999, 25,
+      3, 9,
+      4.9999999999999, 12,
+      5, 4,
+      7.9999999999999, 20,
+      8, 10,
+      10.9999999999999, 25,
+      11, 60,
+      13.9999999999999, 200,
+      14, 200,
+      16.9999999999999, 1000,
+      17, 1000,
+      19.9999999999999, 1500,
+      20, 2000
+    ],
+    "circle-blur": 1,
+    "circle-stroke-color": "#fff",
+    "circle-stroke-opacity": 0
+  },
+  "metadata": {
+    "collection": "demo_birdtracking-movebank",
+    "collection-display-name": "demo_birdtracking-movebank"
+  },
+  "filter": [
+    "all",
+    [
+      "all"
+    ]
+  ]
+}
