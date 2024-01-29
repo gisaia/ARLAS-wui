015d541d16cd2c2184941dd57ea64a5587074a17
Mickael Audel <mickael.audel@gisaia.com>
2024-01-29 10:44:44 +0100

    work: improved visualisation for fine granularity

--- src/config_improved.ts
+++ src/config_improved.ts
@@ -68,7 +68,7 @@ const heatmap = {
 }
 
 
-const cirlce_fines = {
+const cirlce_finest = {
   "id": "arlas_id:Gathered birds positions circle:1706023503869",
   "type": "circle",
   "source": "cluster-track.location-Finest-geohash-cell_center-demo_birdtracking-movebank",
@@ -169,3 +169,98 @@ const cirlce_fines = {
     ]
   ]
 }
+
+
+const cirlce_fines = {
+  "id": "arlas_id:heatmap-circle-fine:1706519948388",
+  "type": "circle",
+  "source": "cluster-track.trail_geohashes_6-Fine-geohash-cell_center-demo_ais_flow",
+  "minzoom": 0,
+  "maxzoom": 24,
+  "layout": {
+    "visibility": "visible",
+    "circle-sort-key": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0, 0,
+      0.2, 2,
+      0.4, 4,
+      0.6, 8
+    ]
+  },
+  "paint": {
+    "circle-opacity": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0, 0.2,
+      1, 0.5
+    ],
+    "circle-color": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      [
+        "get",
+        "count_:normalized"
+      ],
+      0,
+      "#cee897",
+      0.125,
+      "#9dc95b",
+      0.25,
+      "#4bc71e",
+      0.3,
+      "#1bc473",
+      0.35,
+      "#16ccc9",
+      0.4,
+      "#3619cf",
+      0.55,
+      "#9317d1",
+      0.8,
+      "#d11799",
+      1,
+      "#cf1919"
+    ],
+    "circle-radius": [
+      "interpolate",
+      [
+        "linear"
+      ],
+      ["zoom"],
+      0, 10,
+      1.999, 30,
+      4.999999, 90,
+      5, 30,
+      7.999999, 120,
+      8, 40,
+      10.499999, 180,
+      10.5, 30,
+      13.999999, 210,
+      14, 300,
+      16.999999, 1000,
+      17, 2000
+    ],
+    "circle-blur": 1,
+    "circle-stroke-width": 0,
+    "circle-stroke-color": "#fff",
+    "circle-stroke-opacity": 0
+  }
+}
+
+const cirlce_med = {};
+
+
