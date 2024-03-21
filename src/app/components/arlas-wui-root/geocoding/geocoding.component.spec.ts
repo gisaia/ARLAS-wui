11b10e78209f435e3a2c5a84d2dbf362bcb44fd6
MAudelGisaia <mickael.audel@gisaia.com>
2024-03-21 10:38:57 +0100

    fix: put toolkit beta version in package json and fix test and import

--- src/app/components/arlas-wui-root/geocoding/geocoding.component.spec.ts
+++ src/app/components/arlas-wui-root/geocoding/geocoding.component.spec.ts
@@ -1,6 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GeocodingComponent } from './geocoding.component';
+import {GeocodingComponent} from './geocoding.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('GeocodingComponent', () => {
   let component: GeocodingComponent;
@@ -8,6 +16,9 @@ describe('GeocodingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule,  MatInputModule, MatIconModule, BrowserAnimationsModule,
+        TranslateModule.forRoot({ loader: { provide: TranslateLoader, useClass: TranslateFakeLoader } }),
+        MatFormFieldModule, ReactiveFormsModule, MatTableModule],
       declarations: [ GeocodingComponent ]
     })
       .compileComponents();
