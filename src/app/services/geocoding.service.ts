8704b9cf4f16e4010c2663786df133027f769c3b
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2024-08-02 15:13:46 +0200

    ci: add license check

--- src/app/services/geocoding.service.ts
+++ src/app/services/geocoding.service.ts
@@ -17,12 +17,11 @@
  * under the License.
  */
 
-
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {ArlasSettingsService} from 'arlas-wui-toolkit';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ArlasSettingsService } from 'arlas-wui-toolkit';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface GeocodingQueryParams {
   q: string;
