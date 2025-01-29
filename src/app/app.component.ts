b6504088af2758126436b9bd4ee85da506dd29e8
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2025-01-29 15:15:55 +0100

    fix: move initialization of contributors to constructor of arlas-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CollectionReferenceParameters } from 'arlas-api';
 import { ArlasColorService } from 'arlas-web-components';
 import { ResultListContributor } from 'arlas-web-contributors';
@@ -37,7 +37,7 @@ import { ArlasMapFrameworkService } from 'arlas-map';
  *  S: a source class/interface.
  *  M: a Map configuration class/interface.
  */
-export class ArlasWuiComponent<L, S, M> implements OnInit {
+export class ArlasWuiComponent<L, S, M> implements OnInit, OnChanges {
 
   public collections = new Array<string>();
   public collectionToDescription = new Map<string, CollectionReferenceParameters>();
@@ -65,57 +65,32 @@ export class ArlasWuiComponent<L, S, M> implements OnInit {
     private readonly mapFrameworkService: ArlasMapFrameworkService<L, S, M>,
     private readonly colorService: ArlasColorService,
     private readonly collaborativeService: ArlasCollaborativesearchService,
-    private readonly analyticsService: AnalyticsService
-  ) { }
+    private readonly analyticsService: AnalyticsService,
+  ) {
+    // Initialize the contributors and app wide services
+    if (this.arlasStartupService.shouldRunApp && !this.arlasStartupService.emptyMode) {
+      this.collections = [...new Set(Array.from(this.collaborativeService.registry.values()).map(c => c.collection))];
+
+      /** Resultlist */
+      this.initializeResultlist();
+      /** Map */
+      this.initializeMap();
+      /** Analytics */
+      this.initializeAnalytics();
+    }
+  }
 
   public ngOnInit(): void {
     const loadingGif = document.querySelector('.gif');
-    if (!!loadingGif) {
+    if (loadingGif) {
       loadingGif.remove();
     }
 
     // Initialize the contributors and app wide services
     if (this.arlasStartupService.shouldRunApp && !this.arlasStartupService.emptyMode) {
-      this.collections = [...new Set(Array.from(this.collaborativeService.registry.values()).map(c => c.collection))];
-
-      /** Map */
-      // Set MapContributors
-      const mapContributors = [];
-      this.contributorService.getMapContributors().forEach(mapContrib => {
-        mapContrib.colorGenerator = this.colorService.colorGenerator;
-        if (!!this.resultlistContributors) {
-          const resultlistContrbutor: ResultListContributor = this.resultlistContributors
-            .find(resultlistContrib => resultlistContrib.collection === mapContrib.collection);
-          if (!!resultlistContrbutor) {
-            mapContrib.searchSize = resultlistContrbutor.pageSize;
-            mapContrib.searchSort = resultlistContrbutor.sort;
-          } else {
-            mapContrib.searchSize = 50;
-          }
-        }
-        mapContributors.push(mapContrib);
-      });
-      this.mapService.setContributors(mapContributors);
-
-      /** Resultlist */
-      /** Retrieve displayable resultlists */
-      const hiddenListsTabsSet = new Set(this.hiddenResultlistTabs);
-      const allResultlists = this.configService.getValue('arlas.web.components.resultlists');
-      const allContributors = this.configService.getValue('arlas.web.contributors');
-      const resultListsConfig = !!allResultlists ? allResultlists.filter(a => {
-        const contId = a.contributorId;
-        const tab = allContributors.find(c => c.identifier === contId).name;
-        return !hiddenListsTabsSet.has(tab);
-      }) : [];
-
-      const ids = new Set(resultListsConfig.map(c => c.contributorId));
-      this.arlasStartupService.contributorRegistry.forEach((v, k) => {
-        if (v instanceof ResultListContributor) {
-          v.updateData = ids.has(v.identifier);
-          this.resultlistContributors.push(v);
-        }
-      });
-      this.resultlistService.setContributors(this.resultlistContributors, resultListsConfig);
+      if (this.mapService.mapComponent) {
+        this.mapFrameworkService.fitMapBounds(this.mapService.mapComponent.map);
+      }
 
       zip(...this.collections.map(c => this.collaborativeService.describe(c)))
         .pipe(takeUntil(this._onDestroy$))
@@ -124,18 +99,66 @@ export class ArlasWuiComponent<L, S, M> implements OnInit {
             this.collectionToDescription.set(cdr.collection_name, cdr.params);
           });
           this.resultlistService.setCollectionsDescription(this.collectionToDescription);
-          if (this.mapService.mapComponent) {
-            this.mapFrameworkService.fitMapBounds(this.mapService.mapComponent.map);
-          }
           if (this.resultlistContributors.length > 0) {
             this.resultlistContributors.forEach(c => c.sort = this.collectionToDescription.get(c.collection).id_path);
           }
         });
-
-      /** Analytics */
-      const hiddenAnalyticsTabsSet = new Set(this.hiddenAnalyticsTabs);
-      const allAnalytics = this.arlasStartupService.analytics;
-      this.analyticsService.initializeGroups(!!allAnalytics ? allAnalytics.filter(a => !hiddenAnalyticsTabsSet.has(a.tab)) : []);
     }
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hiddenResultlistTabs']?.currentValue !== changes['hiddenResultlistTabs']?.previousValue) {
+      this.initializeResultlist();
+      // Based on which resultlist are defined, there could be a change in the sort of map elements
+      this.initializeMap();
+    }
+    if (changes['hiddenAnalyticsTabs']?.currentValue !== changes['hiddenAnalyticsTabs']?.previousValue) {
+      this.initializeAnalytics();
+    }
+  }
+
+  private initializeResultlist() {
+    const hiddenListsTabsSet = new Set(this.hiddenResultlistTabs);
+    const allResultlists = this.configService.getValue('arlas.web.components.resultlists');
+    const allContributors = this.configService.getValue('arlas.web.contributors');
+    const resultListsConfig = (allResultlists ?? []).filter(a => {
+      const contId = a.contributorId;
+      const tab = allContributors.find(c => c.identifier === contId).name;
+      return !hiddenListsTabsSet.has(tab);
+    });
+
+    const ids = new Set(resultListsConfig.map(c => c.contributorId));
+    this.arlasStartupService.contributorRegistry.forEach((v, k) => {
+      if (v instanceof ResultListContributor) {
+        v.updateData = ids.has(v.identifier);
+        this.resultlistContributors.push(v);
+      }
+    });
+    this.resultlistService.setContributors(this.resultlistContributors, resultListsConfig);
+  }
+
+  private initializeMap() {
+    const mapContributors = [];
+    this.contributorService.getMapContributors().forEach(mapContrib => {
+      mapContrib.colorGenerator = this.colorService.colorGenerator;
+      if (this.resultlistContributors) {
+        const resultlistContrbutor: ResultListContributor = this.resultlistContributors
+          .find(resultlistContrib => resultlistContrib.collection === mapContrib.collection);
+        if (resultlistContrbutor) {
+          mapContrib.searchSize = resultlistContrbutor.pageSize;
+          mapContrib.searchSort = resultlistContrbutor.sort;
+        } else {
+          mapContrib.searchSize = 50;
+        }
+      }
+      mapContributors.push(mapContrib);
+    });
+    this.mapService.setContributors(mapContributors);
+  }
+
+  private initializeAnalytics() {
+    const hiddenAnalyticsTabsSet = new Set(this.hiddenAnalyticsTabs);
+    const allAnalytics = this.arlasStartupService.analytics;
+    this.analyticsService.initializeGroups((allAnalytics ?? []).filter(a => !hiddenAnalyticsTabsSet.has(a.tab)));
   }
 }
