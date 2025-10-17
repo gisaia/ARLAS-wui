5f2be5fb5017a8da050a6c7aed5de75f88613fe3
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2025-10-17 09:53:58 +0200

    test: correct cog pixel component tests

--- src/app/components/map/cog-pixel-legend/cog-pixel-legend.component.spec.ts
+++ src/app/components/map/cog-pixel-legend/cog-pixel-legend.component.spec.ts
@@ -1,5 +1,10 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { ArlasStartupService } from 'arlas-wui-toolkit';
+import { ContributorService } from '../../../services/contributors.service';
+import { VisualizeService } from '../../../services/visualize.service';
+import { MockArlasStartupService } from '../../../tools/test';
 import { CogPixelLegendComponent } from './cog-pixel-legend.component';
 
 describe('CogPixelLegendComponent', () => {
@@ -8,9 +13,26 @@ describe('CogPixelLegendComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CogPixelLegendComponent]
+      imports: [
+        CogPixelLegendComponent,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        })
+      ],
+      providers: [
+        provideHttpClient(),
+        VisualizeService,
+        ContributorService,
+        {
+          provide: ArlasStartupService,
+          useClass: MockArlasStartupService
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CogPixelLegendComponent);
     component = fixture.componentInstance;
