4fc9a71753dafc8b8fc16fc208e6298e6b48cad3
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-04-07 15:27:51 +0200

    build(deps): bump angular to v21

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      
     ],
     client:{
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
