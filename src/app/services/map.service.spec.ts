fb9be9f84d32326420b5a26a596b3ae12dce1931
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-09-07 16:21:28 +0200

    chore: fix MapService test

--- src/app/services/map.service.spec.ts
+++ src/app/services/map.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateLoader, TranslateModule, TranslateNoOpLoader } from '@ngx-translate/core';
+import { ArlasStartupService } from 'arlas-wui-toolkit';
 import { beforeEach, describe, expect, it } from 'vitest';
+import { MockArlasStartupService } from '../tools/test';
 import { ArlasWuiMapService } from './map.service';
 
 describe('MapService', () => {
   let service: ArlasWuiMapService<any, any, any>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateNoOpLoader }
+        })
+      ],
+      providers: [
+        {
+          provide: ArlasStartupService,
+          useClass: MockArlasStartupService
+        }
+      ]
+    });
     service = TestBed.inject(ArlasWuiMapService);
   });
 
